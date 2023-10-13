50996d7ef6a8cd2013360f51207c55a866b44329
shubhamatkal <shubhamatkal333@gmail.com>
2023-10-13 22:26:15 +0530

    Add header

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ window.geometry("1345x680")
 window.title("Typing Speed And Accuracy Test")
 window.configure(bg = "azure2")
 
-
+#TODO Home page for application ================================
+#heading label
+header = Label(window,
+            text= 'Select Paragraph For Test',
+            font= 'rockwell 25 bold underline'
+            , fg= 'black')
+header.grid(row=0 , column=0 , columnspan=3, pady=20)
 
 
 
