3a3775787a17455ae909d44c9fc518670c3ff850
shubhamatkal <shubhamatkal333@gmail.com>
2023-10-13 22:16:59 +0530

    Add Imports and Create Window

---
+++ main.py
@@ -0,0 +1,18 @@
+#imports
+from tkinter import *
+
+#creating tkinter window
+window = Tk()
+window.geometry("1345x680")
+window.title("Typing Speed And Accuracy Test")
+window.configure(bg = "azure2")
+
+
+
+
+
+
+
+
+#end code
+window.mainloop()
