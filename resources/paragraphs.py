9795e7339e16b61c6747dbad198bb20f2df4342c
shubhamatkal <shubhamatkal333@gmail.com>
2023-10-13 22:50:27 +0530

    Add sample paragraphs.py

---
+++ resources/paragraphs.py
@@ -0,0 +1,23 @@
+indian_mythology_paragraphs = {
+    "Ramayana": "The Ramayana is one of the most famous and revered epic poems of Indian mythology. It tells the story of Lord Rama, his wife Sita, and his loyal companion Hanuman. The epic narrates the struggles, adventures, and battles of Rama as he embarks on a quest to rescue Sita from the demon king Ravana.",
+
+    "Mahabharata": "The Mahabharata is another significant epic that revolves around the Kurukshetra war and the events leading up to it. It is a vast and complex narrative that explores themes of dharma, duty, and the consequences of actions. The Bhagavad Gita, a part of the Mahabharata, is a revered spiritual text.",
+
+    "Krishna's Childhood": "One of the most beloved stories in Indian mythology is the childhood of Lord Krishna. It includes his playful antics, his love for butter, and his divine miracles. Krishna's childhood is a source of joy and inspiration for millions of devotees.",
+
+    "Goddess Durga": "Goddess Durga is a powerful and fierce deity in Hindu mythology. She is often depicted riding a lion and is celebrated during the festival of Navratri. Durga symbolizes the triumph of good over evil and the strength of the divine feminine.",
+
+    "The Churning of the Ocean": "The story of the churning of the ocean, known as the Samudra Manthan, is a tale of gods and demons working together to obtain the nectar of immortality. This mythological event is full of symbolism and represents the eternal struggle between the forces of light and darkness.",
+
+    "Lord Shiva": "Lord Shiva, the god of destruction and transformation, is a central figure in Indian mythology. He is often depicted with a third eye, a serpent around his neck, and a crescent moon on his head. Shiva's cosmic dance, known as the Tandava, signifies the cycle of creation and destruction.",
+
+    "The Story of Ravana": "Ravana, the antagonist in the Ramayana, is a complex character in Indian mythology. He was a learned scholar and a devotee of Lord Shiva, but his arrogance and desire for power led to his downfall. The story of Ravana serves as a cautionary tale about the consequences of ego and ambition.",
+
+    "The Legend of Vishnu's Avatars": "Vishnu, one of the principal deities in Hinduism, is believed to have incarnated in various forms (avatars) to restore balance and righteousness in the world. Some of his famous avatars include Lord Rama and Lord Krishna.",
+
+    "The Tale of Arjuna and the Bird's Eye": "In the Mahabharata, there's a famous story about Arjuna and his archery skills. He once had to shoot the eye of a bird while only looking at its reflection in a vessel of water. This story highlights the precision and concentration required in archery and life.",
+
+    "The Creation of the Universe": "Hindu mythology provides various cosmogonic myths that explain the creation of the universe. These myths involve deities like Brahma, who is believed to have created the world from the cosmic egg, and the concept of cycles of creation and destruction.",
+
+    "Ganesha and the Broken Tusk": "Ganesha, the elephant-headed god, is known as the remover of obstacles. He is often depicted with a broken tusk, which has an interesting story behind it. It emphasizes wisdom, sacrifice, and the importance of overcoming challenges.",
+}
